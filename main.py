0533062757e6a28b9a6d83536fb4b06b4b23fd24
AntonioBrightside <JilooPn/1013?>
2024-02-15 14:18:06 +0300

    Made connection to API and downloading *csv file function

--- main.py
+++ main.py
@@ -1,19 +1,8 @@
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
+from source.DownloadDataFromGS import *
 
-scopes = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive"
-]
+if __name__ == "__main__":
 
-credentials = ServiceAccountCredentials.from_json_keyfile_name(
-    r"D:\1. Other\1. Work\1. Programming\# keys\Google API\continual-block-407808-f16acc48f950.json", scopes=scopes)
+    # Download EC data from Google SpreadSheets
+    downloadDataFromGStoCSV(getAPIConnection(scopes, spreadSheet))
 
-file = gspread.authorize(credentials)
-wb = file.open("Jetstat_отчет")
-sheet = wb.get_worksheet(2)
 
-# print(sheet)
-for cell in sheet.range("A1:C5"):
-    print(cell.value)
-    
---
+++ source/DownloadDataFromGS.py
@@ -0,0 +1,56 @@
+import gspread
+import pandas
+from oauth2client.service_account import ServiceAccountCredentials
+
+scopes = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive"
+]
+
+# ECenter data. Jetstat statistics
+spreadSheet = "1hkFVZAI8xjH17fKyMhzpTzLX6jn26JGdH-maHz36o0U"
+
+
+def getAPIConnection(scopes, spreadSheet):
+    """
+    Get connection to API Google SpreadSheets
+    :return: worksheet
+    """
+    scopes = [
+        "https://www.googleapis.com/auth/spreadsheets",
+        "https://www.googleapis.com/auth/drive"
+    ]
+
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(
+        r"D:\1. Other\1. Work\1. Programming\# keys\Google API\continual-block-407808-f16acc48f950.json",
+        scopes=scopes)
+
+    file = gspread.authorize(credentials)
+    wb = file.open("Jetstat_отчет")
+    worksheet = wb.get_worksheet(0)
+
+    return worksheet
+
+
+def downloadDataFromGStoCSV(worksheet):
+    """
+    Download *csv data from 'Jetstat' tab to 'data' directory
+    """
+    dataFrame = pandas.DataFrame(worksheet.get_all_records())
+
+    dataFrame.to_csv(r"D:\1. Other\1. Work\1. Programming\1. Projects\EdgeCenter Google Docs stat\data\jetstat.csv",
+                     index=False,
+                     header=True,
+                     encoding="utf-8")
+
+
+def downloadDataFromGStoXLSX(worksheet):
+    """
+    Download *xlsx data from 'Jetstat' tab to 'data' directory. Works much slower than *csv variant (~3-6 times)
+    """
+    dataFrame = pandas.DataFrame(worksheet.get_all_records())
+
+    dataFrame.to_excel(r"D:\1. Other\1. Work\1. Programming\1. Projects\EdgeCenter Google Docs stat\data\jetstat.xlsx",
+                       index=False,
+                       header=True,
+                       sheet_name="Jetstat")
